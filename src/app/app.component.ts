08f8b2691db07b8481c22188433c34d6324bfde7
itsmanny <info@manuelcampana.com>
2017-08-23 15:40:30 +0100

    Added explanatory comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,13 +11,13 @@ export class AppComponent {
   title = 'Bowling Game';
 
   score: number = 0;
-  rollCount: number = 0;
-  framePos: number = 1;
-  currentFrame: number = 1;
-  rolls:Array<any> = [];
-  scores:Array<any> = [];
-  nextRollIsLast: boolean = false;
-  lastFrame: boolean = false;
+  rollCount: number = 0; // rolls counter
+  framePos: number = 1; // frame position
+  currentFrame: number = 1; // frame of the current roll
+  rolls:Array<any> = []; // array of rolls
+  scores:Array<any> = []; // array of scores
+  nextRollIsLast: boolean = false; // is next roll the last of frame?
+  lastFrame: boolean = false; // is this the last frame?
 
   constructor (
     private scoringService: ScoringService
@@ -26,6 +26,7 @@ export class AppComponent {
   roll(score) {
 
     let endOfGame = () => {
+      // TODO: complete this method
       return this.nextRollIsLast && this.lastFrame;
     };
 
@@ -48,62 +49,58 @@ export class AppComponent {
     this.scores.push(score);
   }
 
-  isSpare(score) {
+  isSpare(score) { // check if it's a spare
     return score + this.scores[this.scores.length - 1] === 10;
   }
 
-  isStrike(score) {
+  isStrike(score) { // check if it's a strike
     return score === 10;
   }
 
-  isLastFrame(framePos) {
+  isLastFrame(framePos) { // check if current frame is last frame
     return framePos > 18;
   }
 
-  isLastRollOfFrame() {
+  isLastRollOfFrame() { // check if current roll is last of frame
     return this.framePos % 2 === 0;
   }
 
-  checkSpare(score) {
+  checkSpare(score) { // if it's a spare add '/' to scores array
     if (this.isSpare(score)){
       this.scores.push('/');
     } else {
       this.scores.push(score);
     };
+    // move to next frame
     this.currentFrame ++;
     this.framePos ++;
   }
 
-  scoreCurrentRoll(score) {
+  scoreCurrentRoll(score) { // attribute correct points for current roll
 
     if (this.isLastFrame(this.framePos)) {
       this.lastFrame = true;
-      if (this.framePos === 20) {
+      if (this.framePos === 20) { // TODO: handle different behaviours of last frame
         console.log("2")
       } else if (this.framePos === 21) {
         console.log("3")
       }
       this.pushScore(score);
       this.framePos ++
-    } else if (this.isStrike(score)) {
+    } else if (this.isStrike(score)) { // if it's a strike add 'X' to scores array and move to next frame
       this.pushScore(score);
       this.scores.push('X');
       this.currentFrame ++;
       this.framePos += 2;
-    } else if (this.isLastRollOfFrame()) {
+    } else if (this.isLastRollOfFrame()) { // if it's second roll of frame, check if it's a spare
       this.nextRollIsLast = false;
       this.checkSpare(score);
-    } else {
+    } else { // simply add points and increase framePos for other scenarios
       this.nextRollIsLast = true;
       this.pushScore(score);
       this.framePos ++;
     }
     this.rollCount ++;
-
-    // console.log('rollCount: ' + this.rollCount);
-    // console.log('framePos: ' + this.framePos);
-    // console.log(this.scores);
-    // console.log(this.currentFrame);
 
   }
 
--- src/app/scoring.service.ts
+++ src/app/scoring.service.ts
@@ -9,33 +9,42 @@ export class ScoringService {
     var score = 0;
     var frameStart = 0;
 
+    // check if the score is 10
     let isStrike = () => {
       return rolls[frameStart] === 10;
     };
 
+    // check if the sum if the score + the previous score is 10
     let isSpare = () => {
       return rolls[frameStart] + rolls[frameStart + 1] === 10;
     };
 
+    // total points of frame
     let framePoints = () => {
       return rolls[frameStart] + rolls[frameStart + 1] || 0;
     };
 
+    // extra points awarded for a strike
     let strikeBonus = () => {
       return rolls[frameStart + 1] + rolls[frameStart + 2] || 0;
     };
 
+    // extra points awarded for a spare
     let spareBonus = () => {
       return rolls[frameStart + 2] || 0;
     };
 
+    // loop through the frames
     for (var i = 0; i < frame; i ++){
+      // if it's a strike add 10pts + strikeBonus
       if(isStrike()){
         score += 10 + strikeBonus();
         frameStart ++;
+      // if it's a spare add 10pts + spareBonus
       } else if (isSpare()){
         score += 10 + spareBonus();
         frameStart += 2;
+      // otherwise add the points of frame
       } else {
         score += framePoints();
         frameStart += 2;
